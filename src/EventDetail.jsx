d5ff79384de1d79aee237b8827be49073d65ae34
Javier Garcia <info@javiergarcia-design.com>
2018-09-12 20:01:00 +0200

    Basic visualization of event details

--- src/EventDetail.jsx
+++ src/EventDetail.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	padding: 20px 50px;
+`;
 
 class EventDetail extends React.Component {
 	state = {
@@ -14,10 +19,22 @@ class EventDetail extends React.Component {
       {
         getEvent(id: "${match.params.id}") {
           id
-          name
+					name
+					admin_name
+					datetime
+					place
+					vehicles {
+						id
+						driver_name
+						free_seats
+						passengers {
+							id
+							name
+						}
+					}
         }
       }
-    `;
+		`;
 
 		axios
 			.post('http://localhost:3000/graphql', {
@@ -33,7 +50,35 @@ class EventDetail extends React.Component {
 	render() {
 		const { event } = this.state;
 
-		return <h1>{event != null ? event.name : 'Loading...'}</h1>;
+		if (event === null) {
+			return <h1>Loading...</h1>;
+		}
+
+		const datetimeString = new Date(parseInt(event.datetime, 10)).toLocaleString();
+
+		return (
+			<Wrapper>
+				<h1>{event.name}</h1>
+				<p>(admin: {event.admin_name})</p>
+				<p>When: {datetimeString}</p>
+				<p>Where: {event.place}</p>
+				<p>Vehicles: {event.vehicles.length > 0 ? event.vehicles.length : 'no vehicles yet'}</p>
+				<ul>
+					{event.vehicles.map(vehicle => (
+						<li key={vehicle.id}>
+							<h2>Driver name: {vehicle.driver_name}</h2>
+							<span>({vehicle.free_seats} free seats)</span>
+							<p>Passengers: {vehicle.passengers.length}</p>
+							<ul>
+								{vehicle.passengers.map(passenger => (
+									<li key={passenger.id}>{passenger.name}</li>
+								))}
+							</ul>
+						</li>
+					))}
+				</ul>
+			</Wrapper>
+		);
 	}
 }
 
